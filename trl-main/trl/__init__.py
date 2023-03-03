3b372ae5f5ec7b622768878455012f638f94f77f
Ajibade Yeseer <123657184+yeseer123@users.noreply.github.com>
2023-03-03 21:23:29 +0100

    Add files via upload

---
+++ trl-main/trl/__init__.py
@@ -0,0 +1,11 @@
+# flake8: noqa
+
+__version__ = "0.2.2.dev0"
+
+from .models import (
+    AutoModelForCausalLMWithValueHead,
+    AutoModelForSeq2SeqLMWithValueHead,
+    PreTrainedModelWrapper,
+    create_reference_model,
+)
+from .trainer import PPOConfig, PPOTrainer
